e01e3c173d3eb0cdc9b44639d6b3cffb878135a5
raz0rcoder <jeff@pixvfm.com>
2024-06-28 20:13:31 -0700

    rebase 2

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'dc0c016d857de7c90bbfd102768a31623b63f389', queries });
-export default client;
-  
